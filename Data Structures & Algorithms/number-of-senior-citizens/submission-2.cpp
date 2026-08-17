13ca7816c9787429b2db16e173eb133826e7eca2
Ayush Saroj <ayushsaroj140@gmail.com>
2026-08-17 19:01:18 +0530

    Add: number-of-senior-citizens - submission-2

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ctr=0;
+        for (auto x : details){
+            if(x[11] > '6' || (x[11] == '6' && x[12] > '0')) ctr++;
+            }
+        return ctr;
+    }
+};
