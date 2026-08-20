985faa1483295a1d1ddc0b2d4dabdb4e180ea278
Ayush Saroj <ayushsaroj140@gmail.com>
2026-08-20 11:43:03 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+   public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> v;
+
+for(int i = 0; i < nums1.size(); i++) {
+
+    for(int j = 0; j < nums2.size(); j++) {
+
+        if(nums1[i] == nums2[j]) {
+
+            bool found = false;
+
+            for(int k = j + 1; k < nums2.size(); k++) {
+
+                if(nums2[k] > nums2[j]) {
+                    v.push_back(nums2[k]);
+                    found = true;
+                    break;
+                }
+            }
+
+            if(!found)
+                v.push_back(-1);
+
+            break;
+        }
+    }
+}
+
+return v;
+    }
+};
